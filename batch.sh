949516da65d255bad180fa2bc473a6d57eb2370d
Vojtech Sykora <vojtasykor@gmail.com>
2023-02-24 17:24:42 +0100

    slurm batch update

--- batch.sh
+++ batch.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem-per-cpu 32G
 #SBATCH --partition amdgpu
 #SBATCH --gres gpu:1
-#SBATCH -o slurm-%j.out # File to which STDOUT will be written
-#SBATCH -e slurm-%j.err # File to which STDERR will be written
+#SBATCH -o slurm/slurm-%j.out # File to which STDOUT will be written
+#SBATCH -e slurm/slurm-%j.err # File to which STDERR will be written
 
 cd /mnt/personal/sykorvo1/PPOthesis/ppo
 singularity run --bind /mnt/personal/sykorvo1:/mnt/personal/sykorvo1 --nv tensorflow_2.10.0-gpu.sif
